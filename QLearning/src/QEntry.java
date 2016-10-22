496203371b5a5cd7dec088952a1be666b282f107
Nicholas Kaminski <nickam12@yahoo.com>
2016-10-21 23:12:28 -0500

    Made Corrections

--- QLearning/src/QEntry.java
+++ QLearning/src/QEntry.java
@@ -1,44 +1,59 @@
-public class QEntry{
-	
-	public enum Action{
-		NORTH,
-		SOUTH,
-		EAST,
-		WEST,
-		PICKUP,
-		DROPOFF
+public class QEntry {
+
+	public enum Action {
+		NORTH, SOUTH, EAST, WEST, PICKUP, DROPOFF
 	}
-	
+
 	State s;
 	Action a;
-	
-	public QEntry(State s, Action a){
+
+	private QEntry(State s, Action a) {
 		this.s = s;
 		this.a = a;
 	}
+	
+	// R(s, a) return immediate reward, where s is some state and a is action
+	public int getImmediateReward() {
+		switch (a) {
+		case PICKUP:
+		case DROPOFF:
+			return 13;
+		default:
+			return -1;
+		}
+	}
 
-	
-	public static QEntry MoveNorth(State s){
-		return new QEntry(s, Action.NORTH);
+	public static QEntry MoveNorth(State s) {
+		State newState = s.clone();
+		return new QEntry(newState, Action.NORTH);
 	}
-	
-	public static QEntry MoveSouth(State s){
-		return new QEntry(s, Action.SOUTH);
+
+	public static QEntry MoveSouth(State s) {
+		State newState = s.clone();
+		return new QEntry(newState, Action.SOUTH);
 	}
-	
-	public static QEntry MoveEast(State s){
-		return new QEntry(s, Action.EAST);
+
+	public static QEntry MoveEast(State s) {
+		State newState = s.clone();
+		return new QEntry(newState, Action.EAST);
 	}
-	
-	public static QEntry MoveWest(State s){
-		return new QEntry(s, Action.WEST);
+
+	public static QEntry MoveWest(State s) {
+		State newState = s.clone();
+		return new QEntry(newState, Action.WEST);
 	}
-	
-	public static QEntry DropOff(State s){
-		return new QEntry(s, Action.DROPOFF);
+
+	public static QEntry DropOff(State s) {
+		State newState = s.clone();
+		return new QEntry(newState, Action.DROPOFF);
 	}
-	
-	public static QEntry PickUp(State s){
-		return new QEntry(s, Action.PICKUP);
+
+	public static QEntry PickUp(State s) {
+		State newState = s.clone();
+		return new QEntry(newState, Action.PICKUP);
+	}
+
+	public String toString() {
+		return new String(s.toString() + a);
 	}
 }
--- QLearning/src/Simulation.java
+++ QLearning/src/Simulation.java
@@ -2,48 +2,56 @@ import java.util.HashMap;
 
 public class Simulation {
 	public enum Occupant {
-		Empty, Pickup, Dropoff
+		Empty, PickUp, DropOff
 	}
 
-	Occupant[][] board;
-	
-	HashMap<QEntry, Float> qtable;
-	
-	State currentState, goalState;
-	
-	public Simulation(){
-		qtable = new HashMap<QEntry, Float>();
-		currentState = new State(1, 5, false, 5, 5, 5, 0, 0, 0);
+	Occupant[][] board = { 
+			{ Occupant.PickUp, Occupant.Empty, Occupant.Empty, Occupant.Empty, Occupant.Empty },
+			{ Occupant.Empty, Occupant.Empty, Occupant.Empty, Occupant.Empty, Occupant.DropOff },
+			{ Occupant.Empty, Occupant.Empty, Occupant.PickUp, Occupant.Empty, Occupant.Empty },
+			{ Occupant.Empty, Occupant.Empty, Occupant.Empty, Occupant.Empty, Occupant.Empty },
+			{ Occupant.DropOff, Occupant.Empty, Occupant.DropOff, Occupant.Empty, Occupant.PickUp } };
+
+	HashMap<QEntry, Double> qtable;
+	double alpha, gamma;
+
+	FullState currentState;
+
+	public Simulation(double learningRate, double discountRate) {
+		alpha = learningRate;
+		gamma = discountRate;
+		qtable = new HashMap<QEntry, Double>();
+		currentState = new FullState(1, 5, 0, 5, 5, 5, 0, 0, 0);
 	}
-	
+
 	/*
-	 * qtable(qentry) = immediate reward for taking action 'a' while in state 's'
-	 * 					+
-	 * 					gamma (or ralitve value of delayed vs. immediate rewards)
-	 * 					*
-	 * 					max qtable(qentry) for all moves after taking action 'a'
+	 * qtable(qentry) = immediate reward for taking action 'a' while in state
+	 * 's' + gamma (or ralitve value of delayed vs. immediate rewards) * max
+	 * qtable(qentry) for all moves after taking action 'a'
 	 */
-	
-	// Q(s, a) look up Q value for being in some state and taking some action
-	public Float Q(QEntry entry){
+	public void updateQTable(QEntry entry){
+		/*
+		double oldUtility = getUtility(entry);
+		double newUtility = (1 - alpha) * oldUtility + alpha * (entry.getImmediateReward() + gamma * getMaxUtilityNextMove(entry));
+		qtable.put(entry, newUtility);
+		*/
+	}
+
+	// Q(s, a) look up Q value for being in some state and taking some action. If a QEntry does not exist add it with a default utility value.
+	public double getUtility(QEntry entry) {
+		if (!qtable.containsKey(entry))
+			qtable.put(entry, 0.0);
 		return qtable.get(entry);
 	}
-	
-	// R(s,a) return immediate reward, where s is some state and a is action
-	public Float R(QEntry entry){
-		return new Float(0);
-	}
-	
-	public void simulate(){
-		while (!currentState.isGoalState()){
-			// decide action
-			// update utility with Q(s,a), where 's' is a state and 'a' is the action to give best utility
-			// make move
+
+	public void simulate(int maxSteps) {
+		for(int i = 0; i < maxSteps; i++){
+			
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		
-		
+		Simulation sim = new Simulation(0.9, 0.1);
+		sim.simulate(1);
 	}
 }
